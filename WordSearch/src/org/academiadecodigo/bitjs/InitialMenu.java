edc39705ebf0d341bbb95701224d6303ebe521e8
Andre Rocha <wwexdx@gmail.com>
2020-10-31 15:52:13 -0100

    added initial menu and some server messages

--- WordSearch/src/org/academiadecodigo/bitjs/InitialMenu.java
+++ WordSearch/src/org/academiadecodigo/bitjs/InitialMenu.java
@@ -1,6 +1,8 @@
 package org.academiadecodigo.bitjs;
 
 import org.academiadecodigo.bootcamp.Prompt;
+import org.academiadecodigo.bootcamp.scanners.integer.IntegerInputScanner;
+import org.academiadecodigo.bootcamp.scanners.integer.IntegerRangeInputScanner;
 import org.academiadecodigo.bootcamp.scanners.string.StringInputScanner;
 
 public class InitialMenu {
@@ -13,21 +15,34 @@ public class InitialMenu {
 
     public static void main(String[] args) {
         InitialMenu menu= new InitialMenu(new GameManager());
-        menu.chooseName();
+        menu.StartMenu();
+
     }
     public InitialMenu(GameManager gameManager){
         this.menuPrompt=new Prompt(System.in,System.out);
         this.gameManager= gameManager;
-    }
 
+    }
 
     public void chooseName (){
         StringInputScanner nameQuestion= new StringInputScanner();
         nameQuestion.setMessage(ServerMessages.nameQuestion);
         String playerName=this.menuPrompt.getUserInput(nameQuestion);
 
+    }
+    public void chooseColor(){
+        IntegerInputScanner colorIntRange = new IntegerRangeInputScanner(1, 5);
+        colorIntRange.setError(ServerMessages.colorError);
+        colorIntRange.setMessage(ServerMessages.colorQuestion);
+        System.out.println(ServerMessages.introColors);
+        System.out.println(ServerMessages.colorOptions);
+        int playerColor= this.menuPrompt.getUserInput(colorIntRange);
 
     }
+    public void StartMenu(){
+        System.out.println(ServerMessages.welcome);
+        chooseName();
+        chooseColor();
 
-
+    }
 }
